e3e59f850ffd7bd4388780888f38d9ed22430716
yinwx0 <3314572822@qq.com>
2024-11-13 14:03:38 +0800

    【1】基础篇之概述及入门案例》

---
+++ 01-base-case/src/main/java/top/wxyin/TestController.java
@@ -0,0 +1,12 @@
+package top.wxyin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public Object test() {
+        return "Hello World!";
+    }
+}
